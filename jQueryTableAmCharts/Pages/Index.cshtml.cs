14c33229a1c8f29a101f6c4d5fa2e28a2ad56866
Andrew Herd <aherd2985@gmail.com>
2020-08-08 23:56:23 -0500

    cleanup

--- jQueryTableAmCharts/Pages/Index.cshtml.cs
+++ jQueryTableAmCharts/Pages/Index.cshtml.cs
@@ -1,16 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using System.Text.Json;
 using static jQueryTableAmCharts.Models.ExampleData;
-using System.Text;
 using System.IO;
 using jQueryTableAmCharts.Models;
-using System.Security.Cryptography.X509Certificates;
 
 namespace jQueryTableAmCharts.Pages
 {
