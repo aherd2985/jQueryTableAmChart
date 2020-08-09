dd2856f48d96d50ab1917e558c7cb503c1a6c749
Andrew Herd <aherd2985@gmail.com>
2020-08-08 23:19:25 -0500

    changed amCharts to use same table as table

--- jQueryTableAmCharts/Models/GraphData.cs
+++ jQueryTableAmCharts/Models/GraphData.cs
@@ -3,26 +3,26 @@ using System.Collections.Generic;
 
 namespace jQueryTableAmCharts.Models
 {
-    public class GraphData
+  public class GraphData
+  {
+    public GraphData()
     {
-        public GraphData()
-        {
-            WPSalary = 0;
-        }
-
-        public string WorkPlace { get; set; }
-        public Int32 WPSalary { get; set; }
-        public IEnumerable<TitleData> TiteList { get; set; }
+      WPSalary = 0;
     }
 
-    public class TitleData
-    {
-        public TitleData()
-        {
-            TitleSalary = 0;
-        }
+    public string WorkPlace { get; set; }
+    public Int32 WPSalary { get; set; }
+    public IEnumerable<TitleData> subData { get; set; }
+  }
 
-        public string Title { get; set; }
-        public Int32 TitleSalary { get; set; }
+  public class TitleData
+  {
+    public TitleData()
+    {
+      value = 0;
     }
+
+    public string name { get; set; }
+    public Int32 value { get; set; }
+  }
 }
--- jQueryTableAmCharts/Pages/Index.cshtml.cs
+++ jQueryTableAmCharts/Pages/Index.cshtml.cs
@@ -10,65 +10,72 @@ using static jQueryTableAmCharts.Models.ExampleData;
 using System.Text;
 using System.IO;
 using jQueryTableAmCharts.Models;
+using System.Security.Cryptography.X509Certificates;
 
 namespace jQueryTableAmCharts.Pages
 {
-    public class IndexModel : PageModel
+  public class IndexModel : PageModel
+  {
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
     {
-        private readonly ILogger<IndexModel> _logger;
-
-        public IndexModel(ILogger<IndexModel> logger)
-        {
-            _logger = logger;
-        }
-
-        public void OnGet()
-        {
-            string readText = string.Empty;
-            using (var sr = new StreamReader("wwwroot/Data/arrays.txt"))
-            {
-                // Set the stream to the string variable
-                readText = sr.ReadToEnd();
-            }
-            Root myDeserializedClass = JsonSerializer.Deserialize<Root>(readText);
-
-            List<Person> personList = new List<Person>();
-            foreach(List<string> personDetail in myDeserializedClass.data)
-            {
-                Person newPerson = new Person();
-                newPerson.WorkPlace = personDetail[2];
-                newPerson.Title = personDetail[1];
-                newPerson.Salary = Int32.Parse(personDetail[5].Substring(1).Replace(",",""));
-                personList.Add(newPerson);
-            }
-
-            List<GraphData> returnGraphData = new List<GraphData>();
-            List<GraphData> salaryGraphData =
-    (from workPlace in personList
-    group workPlace by new { workPlace.WorkPlace, workPlace.Title } into workPlaceGroup
-    select new GraphData
-    {
-        WorkPlace = workPlaceGroup.Key.WorkPlace,
-        WPSalary = workPlaceGroup.Sum(x => x.Salary)
-    }).Cast<GraphData>().ToList();
-            foreach(GraphData graphDataPoint in salaryGraphData)
-            {
-                GraphData returnGraphPoint = salaryGraphData.FirstOrDefault(x => x.WorkPlace == graphDataPoint.WorkPlace);
-                if (returnGraphPoint != null)
-                    returnGraphPoint.TiteList  = (from titlePoint in personList
-                                          where titlePoint.WorkPlace == graphDataPoint.WorkPlace
-                                          group titlePoint by titlePoint.Title into titlePointGroup
-                                          select new TitleData
-                                          {
-                                              Title = titlePointGroup.Key,
-                                              TitleSalary = titlePointGroup.Sum(x => x.Salary),
-                                          }).Cast<TitleData>().ToList();
-                returnGraphData.Add(returnGraphPoint);
-            }
-
-
-           
-
-        }
+      _logger = logger;
     }
+
+    public void OnGet()
+    {
+
+    }
+
+    public List<GraphData> GetGraphData()
+    {
+      string readText = string.Empty;
+      using (var sr = new StreamReader("wwwroot/Data/arrays.txt"))
+      {
+        // Set the stream to the string variable
+        readText = sr.ReadToEnd();
+      }
+      Root myDeserializedClass = JsonSerializer.Deserialize<Root>(readText);
+
+      List<Person> personList = new List<Person>();
+      foreach (List<string> personDetail in myDeserializedClass.data)
+      {
+        Person newPerson = new Person
+        {
+          WorkPlace = personDetail[2],
+          Title = personDetail[1],
+          Salary = Int32.Parse(personDetail[5].Substring(1).Replace(",", ""))
+        };
+        personList.Add(newPerson);
+      }
+
+      List<GraphData> returnGraphData = new List<GraphData>();
+      List<GraphData> salaryGraphData =
+                                        (from workPlace in personList
+                                         group workPlace by workPlace.WorkPlace into workPlaceGroup
+                                         select new GraphData
+                                         {
+                                           WorkPlace = workPlaceGroup.Key,
+                                           WPSalary = workPlaceGroup.Sum(x => x.Salary)
+                                         }).Cast<GraphData>().ToList();
+      foreach (GraphData graphDataPoint in salaryGraphData)
+      {
+        GraphData returnGraphPoint = salaryGraphData.FirstOrDefault(x => x.WorkPlace == graphDataPoint.WorkPlace);
+        if (returnGraphPoint != null)
+          returnGraphPoint.subData = (from titlePoint in personList
+                                       where titlePoint.WorkPlace == graphDataPoint.WorkPlace
+                                       group titlePoint by titlePoint.Title into titlePointGroup
+                                       select new TitleData
+                                       {
+                                         name = titlePointGroup.Key,
+                                         value = titlePointGroup.Sum(x => x.Salary),
+                                       }).Cast<TitleData>().ToList();
+        returnGraphData.Add(returnGraphPoint);
+      }
+      return returnGraphData;
+    }
+
+
+  }
 }
